ae737767f9f90dd473ef3efe199fc5565f5dab87
SleepyHead-Zzzz <matyasjobb@gmail.com>
2026-04-22 15:40:18 +0200

    Refactor sign-up page layout and enhance styling for better user experience

--- pages/signup.php
+++ pages/signup.php
@@ -1,22 +1,24 @@
 <?php require_once '../includes/header.php'; ?>
 <div class="sign-up-card-container">
     <form class="sign-up-card sign-up-card-width" action="post">
-        <h1>Skapa Konto</h1>
+        <div id="h1-skapa-konto-container">
+            <h1>Skapa Konto</h1>
+        </div>
         <div class="sign-up-card-inputs">
             <label for="sign-up-name">Namn</label>
-            <input type="text" id="sign-up-name">
+            <input type="text" id="sign-up-name" placeholder="Skriv in ditt namn">
         </div>
         <div class="sign-up-card-inputs">
             <label for="sign-up-mail">Epost</label>
-            <input type="text" id="sign-up-mail">
+            <input type="text" id="sign-up-mail" placeholder="Skriv in din epost">
         </div>
         <div class="sign-up-card-inputs">
             <label for="sign-up-psw">Lösenord</label>
-            <input type="password" id="sign-up-psw">
+            <input type="password" id="sign-up-psw" placeholder="Minst 8 tecken">
         </div>
         <div class="sign-up-card-inputs">
             <label for="sign-up-psw-confirm">Bekräfta Lösenord</label>
-            <input type="password" id="sign-up-psw-confirm">
+            <input type="password" id="sign-up-psw-confirm" placeholder="Minst 8 tecken">
         </div>
         <div class="sign-up-card-btn-container">
             <input type="button" id="sign-up-cancle-btn" value="Avbryt" onclick="location.href='index.php'">

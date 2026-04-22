c8819e159680a24e14fabe918e5d4bcedd4d8d73
GitTheFOutOfHere <adrian.tornqvist@elev.ga.ntig.se>
2026-04-22 15:53:47 +0200

    added html for footer
    Added a structured footer with contact information and company description.

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,19 @@
-<footer>
-    <p>Detta är en footer</p>
-</footer>
+<link rel="stylesheet" href="./public/style.css">
+
+<body>
+    <footer id="page-footer">
+        <div class="footer-content">
+            <div class="contact-info">
+                <h4>Kontakta oss</h4>
+                <p>📞 073-5642135</p>
+                <p>📧 info@vaktis.se</p>
+            </div>
+
+            <div class="om-oss">
+                <h3>Om oss</h3>
+                <p>Vaktis är en hemsida för personer som vill lägga ut annonser om sin hund/hundar, så att folk kan gå ut och gå med dem. Hemsidan är även till för folk som vill ha ett deltidjobb.</p>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>

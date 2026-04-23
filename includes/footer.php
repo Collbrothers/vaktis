37197b8ca59ef51065d78a9947030a86426e2d1d
GitTheFOutOfHere <adrian.tornqvist@elev.ga.ntig.se>
2026-04-23 13:46:26 +0200

    Replace headings with links in footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,17 +1,19 @@
 <link rel="stylesheet" href="./public/style.css">
+<html lang="sv">
 
 <body>
     <footer id="footer">
         <div class="footer-content">
             <div class="contact-info">
-                <h4>Kontakta oss</h4>
+                <a href="#">Kontakta oss</a>
                 <p>📞 073-5642135</p>
                 <p>📧 info@vaktis.se</p>
             </div>
 
             <div class="om-oss">
-                <h3>Om oss</h3>
-                <p>Vaktis är en hemsida för personer som vill lägga ut annonser om sin hund/hundar, så att folk kan gå ut och gå med dem. Hemsidan är även till för folk som vill ha ett deltidjobb.</p>
+                <a href="#">Om Oss</a>
+                <p>Vaktis är en hemsida för personer som vill lägga ut annonser om sin hund/hundar, så att folk kan gå ut och gå med dem.
+                 Hemsidan är även till för folk som vill ha ett deltidjobb.</p>
             </div>
         </div>
     </footer>

dd1c6778f70f3c7d0b5b8cb24583b16a618568f0
Rico <adrian.tornqvist@elev.ga.ntig.se>
2026-04-29 13:16:07 +0200

    Update footer links

--- includes/footer.php
+++ includes/footer.php
@@ -5,13 +5,13 @@
     <footer id="footer">
         <div class="footer-content">
             <div class="contact-info">
-                <a href="#">Kontakta oss</a>
-                <p>📞 073-5642135</p>
-                <p>📧 info@vaktis.se</p>
-            </div>
+                    <a href="/report" class="contact-link">Kontakta oss</a>
+                    <p>📞 073-5642135</p>
+                    <p>📧 info@vaktis.se</p>
+                </div>
 
             <div class="om-oss">
-                <a href="#">Om Oss</a>
+                <a href="/about" class="om-oss-link">Om oss</a>
                 <p>Vaktis är en hemsida för personer som vill lägga ut annonser om sin hund/hundar, så att folk kan gå ut och gå med dem.
                  Hemsidan är även till för folk som vill ha ett deltidjobb.</p>
             </div>

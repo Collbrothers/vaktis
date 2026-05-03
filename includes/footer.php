c8a6ba666f766a3f46ce4d83c1e566293b5331f2
Aron Kylebäck <aron@kyleback.com>
2026-05-03 14:05:30 +0200

    Revert "Merge pull request #10 from Collbrothers/Footer-Adrian"
    This reverts commit 166f32060e518b38a10852440d05cb898942eba5, reversing
    changes made to 90bf90ceb44cbcebf93e6884bb1f058a92377cb7.

--- includes/footer.php
+++ includes/footer.php
@@ -1,21 +1,5 @@
-<link rel="stylesheet" href="./public/style.css">
-<html lang="sv">
-
-<body>
-    <footer id="footer">
-        <div class="footer-content">
-            <div class="contact-info">
-                    <a href="/report" class="contact-link">Kontakta oss</a>
-                    <p>📞 073-5642135</p>
-                    <p>📧 info@vaktis.se</p>
-                </div>
-
-            <div class="om-oss">
-                <a href="/about" class="om-oss-link">Om oss</a>
-                <p>Vaktis är en hemsida för personer som vill lägga ut annonser om sin hund/hundar, så att folk kan gå ut och gå med dem.
-                 Hemsidan är även till för folk som vill ha ett deltidjobb.</p>
-            </div>
-        </div>
-    </footer>
+<footer>
+    <p>Detta är en footer</p>
+</footer>
 </body>
 </html>

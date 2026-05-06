4f3f7b5e09c4ed3f512794f972d0575599fb8b8e
SleepyHead-Zzzz <matyasjobb@gmail.com>
2026-05-06 13:44:24 +0200

    Added the correct class name to the card in signup.php

--- pages/signup.php
+++ pages/signup.php
@@ -1,7 +1,7 @@
 <?php require_once '../includes/header.php'; ?>
 <main>
     <div class="sign-up-card-container">
-        <form class="login-card sign-up-card-width" action="post">
+        <form class="sign-up-card sign-up-card-width" action="post">
             <div id="h1-skapa-konto-container">
                 <h1>Skapa Konto</h1>
             </div>

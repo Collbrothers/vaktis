4fe2f1307a4abd579170b83f3238bb78e6d7ad01
SleepyHead-Zzzz <matyasjobb@gmail.com>
2026-05-06 13:20:59 +0200

    Tog bort punkter

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>vaktis</title>
     <link rel="stylesheet" href="./public/style.css">
-    <script src="./public/main.js"></script>
+    <script src="/public/main.js"></script>
 </head>
 <body>
     <nav>
@@ -18,7 +18,7 @@ session_start();
             <li><a href="/report">Rapportera</a></li>
             <li><a href="/about">Om Oss</a></li>
             <li><a href="/signup">Skapa Konto</a></li>
-            <li><a href="./login">Logga In</a></li>
+            <li><a href="/login">Logga In</a></li>
         </ul>
         <ul>
             <li><a class="logo-click-on-phone" href="./"><img src="./public/vaktis-logga.png" alt="A logo that with a dog paw followed by the text vaktis"></a></li>
@@ -26,7 +26,7 @@ session_start();
             <li class="hideOnMobile nav-button"><a href="/report">Rapportera</a></li>
             <li class="hideOnMobile nav-button"><a href="/about">Om Oss</a></li>
             <li class="hideOnMobile nav-button-important"><a href="/signup">Skapa Konto</a></li>
-            <li class="hideOnMobile nav-button-important"><a href="./login">Logga In</a></li>
+            <li class="hideOnMobile nav-button-important"><a href="/login">Logga In</a></li>
             <li class="menu-button" onclick="showSidebar()" ><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#f5f0e8"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
         </ul>
     </nav>

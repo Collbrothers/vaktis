3b1622d52ff69c1d457a8a09bf6089b98f4fd44e
SleepyHead-Zzzz <matyasjobb@gmail.com>
2026-05-11 12:32:17 +0200

    Add index page layout with dropdown filters and card components

--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,122 @@
 <?php require_once '../includes/header.php'; ?>
-<h1>vaktis</h1>
+<main>
+    <div class="index-filter-container">
+        <div class="index-dropdown">
+            <button onclick="kommunFunction()" class="index-dropdown-btn">Kommun</button>
+            <div id="kommun-dorpdown" class="index-dropdown-content">
+                <input type="text" placeholder="Sök..." oninput="filterFunction()">
+                <a href="#">Karlsborg</a>
+                <a href="#">Hjo</a>
+                <a href="#">Mariestad</a>
+                <a href="#">Tibro</a>
+                <a href="#">Götborg</a>
+                <a href="#">Skövde</a>
+            </div>
+        </div>
+        <div class="index-dropdown">
+            <button onclick="animalFunction()" class="index-dropdown-btn">Djur</button>
+            <div id="animal-dorpdown" class="index-dropdown-content">
+                <input type="text" placeholder="Sök..." oninput="filterFunctionAnimal()">
+                <a href="#">Cat</a>
+                <a href="#">Dog</a>
+                <a href="#">Elephant</a>
+                <a href="#">Tiger</a>
+                <a href="#">Penguin</a>
+                <a href="#">Giraffe</a>
+            </div>
+        </div>
+    </div>
+    <div class="index-container">
+        <!--Card-->
+        <div class="index-card">
+            <!--<img class="index-card-image"
+                src="https://www.borrowmydoggy.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2F4ij0poqn%2Fproduction%2Fe24bfbd855cda99e303975f2bd2a1bf43079b320-800x600.jpg&w=1080&q=80"
+                alt="Bild över husdjuret">-->
+            <div class="index-card-image-placeholder">Ingen Bild</div> <!--När det inte finns någon bild-->
+
+            <div class="index-text">
+                <h2>Lasse</h2> <!--Namn på djur-->
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia ipsa ad optio, natus ex debitis sed id
+                    minus? Velit saepe quasi aperiam quo accusamus quis, laborum quidem nostrum voluptates magnam.</p> <!--Kort sammanfattning av beskrivningen-->
+            </div>
+
+            <div class="index-button-container"> <!---->
+                <a class="index-button" href="">Läs mer→</a>
+            </div>
+        </div>
+
+        <!--Card-->
+        <div class="index-card">
+            <!--<img class="index-card-image"
+                src="https://www.borrowmydoggy.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2F4ij0poqn%2Fproduction%2Fe24bfbd855cda99e303975f2bd2a1bf43079b320-800x600.jpg&w=1080&q=80"
+                alt="Bild över husdjuret">-->
+            <div class="index-card-image-placeholder">Ingen Bild</div> <!--När det inte finns någon bild-->
+
+            <div class="index-text">
+                <h2>Lasse</h2> <!--Namn på djur-->
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia ipsa ad optio, natus ex debitis sed id
+                    minus? Velit saepe quasi aperiam quo accusamus quis, laborum quidem nostrum voluptates magnam.</p> <!--Kort sammanfattning av beskrivningen-->
+            </div>
+
+            <div class="index-button-container"> <!---->
+                <a class="index-button" href="">Läs mer→</a>
+            </div>
+        </div>
+
+        <!--Card-->
+        <div class="index-card">
+            <!--<img class="index-card-image"
+                src="https://www.borrowmydoggy.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2F4ij0poqn%2Fproduction%2Fe24bfbd855cda99e303975f2bd2a1bf43079b320-800x600.jpg&w=1080&q=80"
+                alt="Bild över husdjuret">-->
+            <div class="index-card-image-placeholder">Ingen Bild</div> <!--När det inte finns någon bild-->
+
+            <div class="index-text">
+                <h2>Lasse</h2> <!--Namn på djur-->
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia ipsa ad optio, natus ex debitis sed id
+                    minus? Velit saepe quasi aperiam quo accusamus quis, laborum quidem nostrum voluptates magnam.</p> <!--Kort sammanfattning av beskrivningen-->
+            </div>
+
+            <div class="index-button-container"> <!---->
+                <a class="index-button" href="">Läs mer→</a>
+            </div>
+        </div>
+
+        <!--Card-->
+        <div class="index-card">
+            <!--<img class="index-card-image"
+                src="https://www.borrowmydoggy.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2F4ij0poqn%2Fproduction%2Fe24bfbd855cda99e303975f2bd2a1bf43079b320-800x600.jpg&w=1080&q=80"
+                alt="Bild över husdjuret">-->
+            <div class="index-card-image-placeholder">Ingen Bild</div> <!--När det inte finns någon bild-->
+
+            <div class="index-text">
+                <h2>Lasse</h2> <!--Namn på djur-->
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia ipsa ad optio, natus ex debitis sed id
+                    minus? Velit saepe quasi aperiam quo accusamus quis, laborum quidem nostrum voluptates magnam.</p> <!--Kort sammanfattning av beskrivningen-->
+            </div>
+
+            <div class="index-button-container"> <!---->
+                <a class="index-button" href="">Läs mer→</a>
+            </div>
+        </div>
+
+        <!--Card-->
+        <div class="index-card">
+            <!--<img class="index-card-image"
+                src="https://www.borrowmydoggy.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2F4ij0poqn%2Fproduction%2Fe24bfbd855cda99e303975f2bd2a1bf43079b320-800x600.jpg&w=1080&q=80"
+                alt="Bild över husdjuret">-->
+            <div class="index-card-image-placeholder">Ingen Bild</div> <!--När det inte finns någon bild-->
+
+            <div class="index-text">
+                <h2>Lasse</h2> <!--Namn på djur-->
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia ipsa ad optio, natus ex debitis sed id
+                    minus? Velit saepe quasi aperiam quo accusamus quis, laborum quidem nostrum voluptates magnam.</p> <!--Kort sammanfattning av beskrivningen-->
+            </div>
+
+            <div class="index-button-container"> <!---->
+                <a class="index-button" href="">Läs mer→</a>
+            </div>
+        </div>
+    </div>
+</main>
+
 <?php require_once '../includes/footer.php'; ?>
--- public/main.js
+++ public/main.js
@@ -7,3 +7,43 @@ function hideSidebar(){
 const sidebar = document.querySelector('.sidebar')
 sidebar.style.display = 'none'
 }
+
+function kommunFunction() {
+  document.getElementById("kommun-dorpdown").classList.toggle("show");
+}
+
+function filterFunctionKommun() {
+  var input, filter, div, a, i, txtValue;
+  input = document.querySelector("#kommun-dorpdown input");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("kommun-dorpdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
+function animalFunction() {
+  document.getElementById("animal-dorpdown").classList.toggle("show");
+}
+
+function filterFunctionAnimal() {
+  var input, filter, div, a, i, txtValue;
+  input = document.querySelector("#kommun-dorpdown input");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("animal-dorpdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}

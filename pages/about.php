ae5e0eecb98782d35a9a4f9a5dcacf9887455dbf
GitTheFOutOfHere <adrian.tornqvist@elev.ga.ntig.se>
2026-05-22 11:36:48 +0200

    Enhance about page layout and styling with new content and responsive design

---
+++ pages/about.php
@@ -0,0 +1,66 @@
+<?php require_once '../includes/header.php'; ?>
+
+
+<!DOCTYPE html>
+<link rel="stylesheet" href="/public/style.css">
+
+
+<div class="about-section">
+    <h1>🐾 OM OSS</h1>
+    <p>
+        Vaktis är en trygg och enkel plats där djurägare kan hitta hjälp med sina husdjur.<br>
+        Här kan du lägga upp annonser om du behöver någon som kan vakta, gå ut och gå eller ta hand om ditt husdjur när du själv inte har möjlighet.<br>
+        Samtidigt kan djurälskare hitta uppdrag och hjälpa till att ge husdjuren den omsorg och motion de behöver.<br>
+         En mötesplats för både djurägare och ansvarsfulla djurvänner!
+    </p>
+</div>
+
+<div class="flex-row">
+   
+    <div class="flex-item">
+        <div class="card-frame">
+            <img src="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" 
+                 alt="Katt som sitter tryggt"
+                 loading="lazy">
+        </div>
+    </div>
+
+
+    <div class="flex-item">
+        <div class="card-frame">
+            <img src="https://thumbs.dreamstime.com/b/golden-retriever-dog-21668976.jpg" 
+                 alt="Golden retriever"
+                 loading="lazy">
+        </div>
+    </div>
+
+    <div class="flex-item">
+        <div class="card-frame">
+            <div class="contact-content">
+                <h3> Kontakta oss</h3>
+                <ul class="contact-list">
+                    <li>
+                        <strong>Telefon:</strong>
+                        <a href="tel:0725188995" class="plain-text">072 518 89 95</a>
+                    </li>
+                    <li>
+                        <strong>Epost:</strong>
+                        <a href="mailto:aron@kyleback.com">aron@kyleback.com</a>
+                    </li>
+                    <li>
+                        <strong>Github:</strong>
+                        <a href="https://github.com/Collbrothers/vaktis" target="_blank" rel="noopener noreferrer">vaktis på Github ⧉</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="footer-note">
+     Trygg och enkel plats för djur och djurälskare – Vaktis
+</div>
+
+</body>
+</html>
+<?php require_once '../includes/footer.php'; ?>
